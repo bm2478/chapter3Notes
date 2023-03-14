e1252412ff47304383bfe6708d942be113bf52ee
7xge <7xge@fresnou.org>
2023-03-14 13:59:50 -0700

    initial commit

---
+++ functionsNotes.js
@@ -0,0 +1,47 @@
+//Functions Notes
+
+//Step 1 - Define the function
+// function sayHello(){
+// console.log('Hello');
+// }
+
+//Step 2 - Call (use) the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+//define
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if (x>y){
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+
+//call
+// printMax();
+
+//define
+// function printMax(x, y){
+//     if (x>y){
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+// printMax(24, 8);
+// printMax(2, 75);
+// printMax(2, 2);
+
+function sum(num1, num2){
+    console.log(`${num1}+${num2}=${num1+num2}`);
+}
+
+sum(7, 10);
+sum(27, 1);
