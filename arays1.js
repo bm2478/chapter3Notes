8ff42a42b5d7382e22234659ff05420bef87f445
7xge <7xge@fresnou.org>
2023-03-24 14:19:03 -0700

    initial commit

---
+++ arays1.js
@@ -0,0 +1,30 @@
+//1
+// function firstElement(arr){
+//     console.log(arr[0]);
+// }
+// let myNums = [1,2,3,4];
+// firstElement (myNums);
+
+//2
+// function firstElement(arr){
+//     console.log(arr[3]);
+// }
+// let myNums = [3,4,5,6];
+// firstElement (myNums);
+
+//3
+function printSecond(arr){
+    if(arr.length == 0){
+        console.log("Empty array");
+    } else{
+        console.log( arr[0] )
+    }
+}
+let myNums = [5, 10, 15, 20];
+//firstElement (myNums);
+printSecond(myNums) ;
+
+let empty = [];
+printSecond(empty) ;
+
+//4
