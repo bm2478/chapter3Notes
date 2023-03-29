a86b7a294295d5f13df133010e788befe8691ecb
7xge <7xge@fresnou.org>
2023-03-29 14:17:07 -0700

    initial commit

---
+++ arays2.js
@@ -0,0 +1,27 @@
+//1
+// function count(n){
+//     for(let i=1; i<=n;i++){
+//         console.log(i)
+//     }
+// }
+//count(99)
+
+//2
+// function count(n){
+//     if(n <=0){
+//         console.log('invalid')
+//     } else{
+//         for(let i=1; i<=n;i++){
+//             console.log(i)
+//         }
+//     }
+// }
+
+//3
+function replaceWithx(arr, x){
+    for(leti=o; i< arr.length; i++){
+        arr[i]=x
+    }
+    console.log(arr)
+}
+replaceWithx(1,2,3,4,7);
