b9345ae76adc99cae9b89b1e972b7ada84de0947
7xge <7xge@fresnou.org>
2023-04-28 13:52:14 -0700

    8ball

--- magic8.js
+++ magic8.js
@@ -1,4 +1,9 @@
+const readLine = require('readline-sync');
 let answers=['yes', 'no', 'maybe', 'never', 'always'];
+
+console.log("Welcome to the 8ball machine!");
+
+readLine.question("Enter a question: ");
 
 let randomNum = Math.floor(Math.random()*answers.length);
 let answer = answers[randomNum];
