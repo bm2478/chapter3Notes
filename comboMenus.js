54e2a43afd1e4825261b701a017255526bfc9aaa
7xge <7xge@fresnou.org>
2023-04-28 14:20:08 -0700

    combomenu 2

--- comboMenus.js
+++ comboMenus.js
@@ -6,3 +6,11 @@ let order = [];
 order.push( readLine.question("What Sandwhich you would like to have?: "));
 console.log(`You ordered: ${order[0]}`);
 
+let order2 = readLine.question("Would you like a drink?: ");
+console.log(order2);
+
+if (answer == "yes"){
+    console.log(readLine.question("What drink size do you want?: "));
+}  else if(answer == "no"){
+    console.log("Oh ok then...")
+}
